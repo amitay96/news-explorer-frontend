3966a69a2621614f806b313f855503e3e75d3dcf
amitay96 <amitay96@gmail.com>
2022-11-09 19:02:42 +0200

    refactor: header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,37 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import logout_icon from "../../images/icons/logout.svg";
 import Navigation from "../Navigation/Navigation";
 import "./Header.css";
 
 const Header = () => {
   return (
     <header className="header">
+      <Navigation />
       <div className="header__container">
         <p className="header__logo">NewsExplorer</p>
-        <Navigation></Navigation>
+        <nav className="header__navigation">
+          <ul className={"header__links"}>
+            <li className="header__link-item header__link-home_active">
+              <Link to="/home" className="header__link">
+                Home
+              </Link>
+            </li>
+            <li className="header__link-item header__link-saved_active">
+              <Link to="/saved" className="header__link">
+                Saved Articles
+              </Link>
+            </li>
+            <li className="header__link-item header__user_login">
+              Sign in
+              <img
+                src={logout_icon}
+                className="header__user_logout-icon"
+                alt="logout icon"
+              />
+            </li>
+          </ul>
+        </nav>
       </div>
     </header>
   );
