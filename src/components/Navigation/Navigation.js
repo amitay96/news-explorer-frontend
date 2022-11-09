43dbf12e83a4722c8b1fdfbe75d70eb6fe593080
amitay96 <amitay96@gmail.com>
2022-11-09 20:49:54 +0200

    refactor: navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,36 +1,28 @@
 import React from "react";
-import logout_icon from "../../images/icons/logout.svg";
-// import { Link } from "react-router-dom";
-// import Menu from "./Menu";
+import { Link } from "react-router-dom";
 import "./Navigation.css";
 
-const Navigation = () => {
+const Navigation = (props) => {
+  const signoutClick = () => {
+    props.toggleMenu();
+    props.handleSignout();
+  };
+
   return (
-    <nav className="navigation">
-      <ul className={"navigation__links"}>
-        <li className="navigation__link-item navigation__link-home_active">
-          {/* <Link to="/home" className="navigation__link"> */}
-          Home
-          {/* </Link> */}
-        </li>
-        <li className="navigation__link-item navigation__link-saved_active">
-          {/* <Link to="/saved" className="navigation__link"> */}
-          Saved Articles
-          {/* </Link> */}
-        </li>
-        <li className="navigation__link-item navigation__user_login">
-          {/* <Link to="/signin" className="navigation__link"> */}
-          Sign in
-          <img
-            src={logout_icon}
-            className="navigation__user_logout-icon"
-            alt="logout icon"
-          />
-          {/* </Link> */}
+    <div
+      className={`header__menu ${
+        props.isMenuOpen ? "header__menu_active" : ""
+      }`}
+    >
+      <ul className="header__links header__links_menu">
+        <li className="header__link-item">{props.name}</li>
+        <li className="header__link-item">
+          <Link to="/signin" className="header__link" onClick={signoutClick}>
+            Log out
+          </Link>
         </li>
       </ul>
-      {/* <Menu></Menu> */}
-    </nav>
+    </div>
   );
 };
 
