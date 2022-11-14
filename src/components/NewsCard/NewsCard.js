24151ac3f9bee5e9d055f609eaf470966637fe76
amitay96 <amitay96@gmail.com>
2022-11-14 21:37:53 +0200

    add: localcards

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -1,28 +1,20 @@
 import React from "react";
-import card from "../../images/card-image.png";
 import "./NewsCard.css";
 
-const NewsCard = (props) => {
+const NewsCard = (newsCard) => {
   return (
     <li className="newsCard">
       <button className="newsCard__save_button" type="button" />
       <img
         className="newsCard__image"
-        src={card}
-        // alt={props.newsCard.name}
+        src={newsCard.image}
+        alt={newsCard.title}
       />
       <div className="newsCard__text-area">
-        <p className="newsCard__date">November 4, 2020</p>
-        <p className="newsCard__title">
-          Everyone Needs a Special 'Sit Spot' in Nature
-        </p>
-        <p className="newsCard__text">
-          Ever since I read Richard Louv's influential book, "Last Child in the
-          Woods," the idea of having a special "sit spot" has stuck with me.
-          This advice, which Louv attributes to nature educator Jon Young, is
-          for both adults and children to find...
-        </p>
-        <p className="newsCard__source">treehugger</p>
+        <p className="newsCard__date">{newsCard.date}</p>
+        <p className="newsCard__title">{newsCard.title}</p>
+        <p className="newsCard__text">{newsCard.text}</p>
+        <p className="newsCard__source">{newsCard.source}</p>
       </div>
     </li>
   );
