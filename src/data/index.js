73afce567f4ab5034e3115624deb868505b324f4
amitay96 <amitay96@gmail.com>
2022-11-16 17:35:59 +0200

    refactor: data

--- src/data/index.js
+++ src/data/index.js
@@ -1,11 +1,17 @@
-const savedArticles = [
+import cardImg_01 from "../images/card-image_01.png";
+import cardImg_02 from "../images/card-image_02.png";
+import cardImg_03 from "../images/card-image_03.png";
+import cardImg_04 from "../images/card-image_04.png";
+import cardImg_05 from "../images/card-image_05.png";
+
+export const savedArticles = [
   {
     keyword: "Nature",
     title: "Everyone Needs a Special 'Sit Spot' in Nature",
     text: `Ever since I read Richard Louv's influential book, "Last Child in the Woods", the idea of having a special "sit spot" has stuck with me. This advice, which Louv attributes to nature educator Jon Young, is for both adults and children to find...`,
     date: "November 4, 2020",
     source: "treehugger",
-    image: "",
+    image: cardImg_01,
   },
   {
     keyword: "Nature",
@@ -13,7 +19,7 @@ const savedArticles = [
     text: `We all know how good nature can make us feel. We have known it for millennia: the sound of the ocean, the scents of a forest, the way dappled sunlight dances through leaves.`,
     date: "February 19, 2019",
     source: "national geographic",
-    image: "",
+    image: cardImg_02,
   },
   {
     keyword: "Yellowstone",
@@ -22,7 +28,7 @@ const savedArticles = [
       "Uri Løvevild Golman and Helle Løvevild Golman are National Geographic Explorers and conservation photographers who just completed a project and book they call their love letter to...",
     date: "October 19, 2020",
     source: "national geographic",
-    image: "",
+    image: cardImg_03,
   },
   {
     keyword: "Parks",
@@ -31,7 +37,7 @@ const savedArticles = [
       "“The linking together of the Cascade and Death Canyon trails, at their heads, took place on October 1, 1933, and marked the first step in the realization of a plan whereby the hiker will be...",
     date: "November 4, 2020",
     source: "National parks traveler",
-    image: "",
+    image: cardImg_04,
   },
   {
     keyword: "Photography",
@@ -40,8 +46,6 @@ const savedArticles = [
       "Humans have long relied on the starry sky to push into new frontiers, sail to the very edge of the world and find their way back home again. Even animals look to the stars to guide them. ",
     date: "March 16, 2020",
     source: "treehugger",
-    image: "",
+    image: cardImg_05,
   },
 ];
-
-module.exports = savedArticles;
