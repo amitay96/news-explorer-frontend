17640956ad7b0a3e88793b6d0b2b22b25d18ddd8
amitay96 <amitay96@gmail.com>
2022-11-23 21:18:59 +0200

    refactor: login popup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -14,35 +14,43 @@ function Login({ isOpen, onClose, onSubmit }) {
         onSubmit={onSubmit}
       >
         <fieldset className="form__fieldset">
-          <label className="form__label">Email</label>
-          <input
-            type="email"
-            name="email"
-            className="form__input"
-            placeholder="Enter email"
-            //   value={values.email}
-            //   onChange={handleChange}
-          />
-          <label className="form__label">Password</label>
-          <input
-            type="password"
-            name="password"
-            className="form__input"
-            placeholder="Enter password"
-            //   value={values.password}
-            //   onChange={handleChange}
-          />
-          <button className="form__button" type="submit">
-            Sign in
-          </button>
+          <label className="form__label">
+            Email:
+            <input
+              type="email"
+              name="email"
+              className="form__input"
+              placeholder="Enter email"
+              id="email-input"
+              //   value={values.email}
+              //   onChange={handleChange}
+            />
+            <span id="name-input-error"></span>
+          </label>
+          <label className="form__label">
+            Password:
+            <input
+              type="password"
+              name="password"
+              className="form__input"
+              placeholder="Enter password"
+              id="password-input"
+              //   value={values.password}
+              //   onChange={handleChange}
+            />
+            <span id="name-input-error"></span>
+          </label>
         </fieldset>
+        <button className="form__button" type="submit">
+          Sign in
+        </button>
       </form>
 
       <p className="form__footer-text">
         or{" "}
-        <Link to="/signup" className="form__footer-link">
+        <span to="/signup" className="form__footer-link" onClick={openSignup}>
           Sign up
-        </Link>
+        </span>
       </p>
     </Popup>
   );
