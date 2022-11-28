0562fd28d63677841d1828aa88419b18aba99294
amitay96 <amitay96@gmail.com>
2022-11-28 20:43:05 +0200

    feature: add location context

---
+++ src/contexts/LocationContext.js
@@ -0,0 +1,24 @@
+import React, { useState, useEffect, createContext, useContext } from "react";
+import { useLocation } from "react-router-dom";
+
+const LocationContext = createContext();
+
+const LocationProvider = ({ children }) => {
+  const [isMain, setIsMain] = useState(true);
+  const location = useLocation();
+
+  useEffect(() => {
+    location.path !== "/" ? setIsMain(false) : setIsMain(true);
+  }, [location]);
+  return (
+    <LocationContext.Provider value={isMain}>
+      {children}
+    </LocationContext.Provider>
+  );
+};
+
+export default LocationProvider;
+
+export const usePopup = () => {
+  return useContext(LocationContext);
+};
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./components/App/App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import LocationContext from "./contexts/LocationContext";
 import PopupContext from "./contexts/PopupContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,7 +12,9 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <PopupContext>
-        <App />
+        <LocationContext>
+          <App />
+        </LocationContext>
       </PopupContext>
     </BrowserRouter>
   </React.StrictMode>
