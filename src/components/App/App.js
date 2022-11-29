a334a47d3533d7ce29dad8ab1df88a94195cb3f8
amitay96 <amitay96@gmail.com>
2022-11-29 15:39:15 +0200

    feature: non found page redirect

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Main from "../Main/Main";
 import SavedNews from "../SavedNews/SavedNews";
 import Footer from "../Footer/Footer";
@@ -12,8 +12,9 @@ function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Main />} />
         <Route path="/saved-news" element={<SavedNews />} />
+        <Route exact={true} path="/" element={<Main />} />
+        <Route path="*" element={<Navigate to="/" />} />
       </Routes>
       <Footer />
       <Signin />
