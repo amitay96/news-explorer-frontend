5a36bf9463f0773a519c02d79df1bef12c119401
amitay96 <amitay96@gmail.com>
2022-12-01 13:38:35 +0200

    fix: scrolling and layout

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -4,7 +4,7 @@ import "./Preloader.css";
 function Preloader() {
   return (
     <div className="preloader">
-      <i className="circle-preloader" />
+      <i className="preloader__animation" />
       <p className="preloader__title">Searching for news...</p>
     </div>
   );
