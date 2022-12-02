656eea07d743a3e67c0ecde0803a83fb73451752
amitay96 <amitay96@gmail.com>
2022-12-02 22:16:57 +0200

    hotfix: preloader layout

--- src/components/Preloader/Preloader.js
+++ src/components/Preloader/Preloader.js
@@ -5,7 +5,7 @@ function Preloader() {
   return (
     <div className="preloader">
       <i className="preloader__animation" />
-      <p className="preloader__title">Searching for news...</p>
+      <p className="preloader__text">Searching for news...</p>
     </div>
   );
 }
