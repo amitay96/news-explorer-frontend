177d1bf98ac2cc55d6bd99eca6edd8e428bd7796
amitay96 <amitay96@gmail.com>
2022-12-09 00:21:48 +0200

    feature: add news API

---
+++ src/utils/NewsApi.js
@@ -0,0 +1,23 @@
+class NewsApi {
+  constructor({ baseUrl, apiKey }) {
+    this._baseUrl = baseUrl;
+    this._apiKey = apiKey;
+  }
+
+  _checkResponse(res) {
+    return res.ok ? res.json() : Promise.reject(res.statusText);
+  }
+
+  getNews(keyword) {
+    return fetch(
+      `${this._baseUrl}${keyword}&apiKey=${this._apiKey}`
+    ).then((res) => this._checkResponse(res));
+  }
+}
+
+const newsApi = new NewsApi({
+  baseUrl: "https://nomoreparties.co/news/v2/everything?q=",
+  apiKey: "ff004cc9e6f2458298010ebb5f31edb5",
+});
+
+export default newsApi;
