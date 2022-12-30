01b59600547df7751f7409f41b8e837d37f9c4bf
amitay96 <amitay96@gmail.com>
2022-12-30 05:47:37 +0200

    feauture: async function

--- src/contexts/entities/useUser.js
+++ src/contexts/entities/useUser.js
@@ -7,27 +7,8 @@ export const useUser = () => {
   const [currentUser, setCurrentUser] = useState({ username: "" });
   const [loggedIn, setLoggedIn] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
-  // const [token, setToken] = useState(localStorage.getItem("jwt"));
 
-  useEffect(() => {
-    const token = localStorage.getItem("jwt");
-    if (token) {
-      setIsLoading(true);
-      UserApi.checkToken(token)
-        .then((res) => {
-          console.log(res);
-          if (res._id) {
-            setLoggedIn(true);
-            setCurrentUser({ username: res.name });
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          history("/");
-        })
-        .finally(() => setIsLoading(false));
-    }
-  }, [history]);
+  // useEffect(() => checkToken(), [loggedIn]);
 
   const checkToken = async () => {
     try {
@@ -45,35 +26,31 @@ export const useUser = () => {
     }
   };
 
-  const handleLogin = (data) => {
+  const handleLogin = async (data) => {
     setIsLoading(true);
-    UserApi.login(data)
-      .then((res) => {
-        if (res) {
-          console.log(res);
-          localStorage.setItem("jwt", res.token);
-          setCurrentUser({ username: res.data.name });
-          setLoggedIn(true);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-      .finally(() => setIsLoading(false));
+    try {
+      const user = await UserApi.login(data);
+      localStorage.setItem("jwt", user.token);
+      setCurrentUser({ username: user.data.name });
+      setLoggedIn(true);
+      return user;
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
-  const handleRegister = (data) => {
+  const handleRegister = async (data) => {
     setIsLoading(true);
-    UserApi.register(data)
-      .then((res) => {
-        if (res) {
-          console.log(res);
-        }
-      })
-      .catch((err) => {
-        console.log(err);
-      })
-      .finally(() => setIsLoading(false));
+    try {
+      const user = await UserApi.register(data);
+      return user;
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setIsLoading(false);
+    }
   };
 
   const handleLogout = () => {
