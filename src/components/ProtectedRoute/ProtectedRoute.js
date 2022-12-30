50f41dc7a2aab0837d8d957e64f3dae74f49c5cf
amitay96 <amitay96@gmail.com>
2022-12-30 05:48:07 +0200

    hotfix: global context

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
-import { useUser } from "../../contexts/UserContext";
+import { useStore } from "../../contexts/GlobalContext";
 
 const ProtectedRoute = ({ children }) => {
-  const { loggedIn } = useUser();
+  const { loggedIn } = useStore().UserActions;
 
   return loggedIn ? children : <Navigate to="/" />;
 };
