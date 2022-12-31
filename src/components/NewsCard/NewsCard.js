302aab75778737146d7cef7452ac6ac32124decc
amitay96 <amitay96@gmail.com>
2022-12-31 20:42:07 +0200

    feature: glabal card button options

--- src/components/NewsCard/NewsCard.js
+++ src/components/NewsCard/NewsCard.js
@@ -5,18 +5,17 @@ import { useStore } from "../../contexts/GlobalContext";
 
 import "./NewsCard.css";
 
-const NewsCard = ({
-  title,
-  text,
-  date,
-  image,
-  source,
-  currentCard,
-}) => {
-  const { isMain } = useLocations();
-  const { loggedIn, savedArticles, handleGetSaved, handleSaveArticle, handleDeleteSaved } = useStore().UserActions;
-  const { keyword } = useStore().NewsActions;
+const NewsCard = ({ title, text, date, image, source, currentCard }) => {
   const popupContext = usePopup();
+  const { isMain } = useLocations();
+  const {
+    loggedIn,
+    savedArticles,
+    handleGetSaved,
+    handleSaveArticle,
+    handleDeleteSaved,
+  } = useStore().UserActions;
+  const { keyword } = useStore().NewsActions;
 
   const dateOptions = {
     month: "short",
@@ -24,60 +23,60 @@ const NewsCard = ({
     year: "numeric",
   };
 
-  const shortDate = new Date(date).toLocaleDateString(
-    "en-US",
-    dateOptions
-  );
+  const shortDate = new Date(date).toLocaleDateString("en-US", dateOptions);
 
   const saveCard = () => {
     handleSaveArticle({
-        title,
-        text,
-        date,
-        image,
-        source,
-        keyword,
-        link: currentCard.url,
+      title,
+      text,
+      date,
+      image,
+      source,
+      keyword,
+      link: currentCard.url,
     });
     handleGetSaved();
-}
+  };
 
-function deleteCardFromSaved(currentCard) {
-  console.log(currentCard);
+  const deleteCardFromSaved = (currentCard) => {
     const cardToDelete = savedArticles.find(
-        (card) => card.title === currentCard.title
+      (card) => card.title === currentCard.title
     );
     handleDeleteSaved(cardToDelete._id);
-}
+  };
 
   const handleCardButton = (card) => {
-    if (isMain && loggedIn) saveCard();
-    else if (isMain && !loggedIn) popupContext.openPopup("signin");
-    else deleteCardFromSaved(card);
+    isMain
+      ? loggedIn
+        ? saveCard()
+        : popupContext.openPopup("signin")
+      : deleteCardFromSaved(card);
   };
-  
+  const isSaved = savedArticles.some(
+    (card) => card.title === currentCard.title
+  );
 
   return (
     <article className="newsCard">
       {!isMain && <p className="newsCard__keyword">{currentCard.keyword}</p>}
       <button
-        className={
+        className={`newsCard__button ${
           isMain
-            ? "newsCard__button newsCard__button_home"
-            : "newsCard__button newsCard__button_saved"
-        }
+            ? !isSaved
+              ? "newsCard__button_home"
+              : "newsCard__button_home-active"
+            : "newsCard__button_saved"
+        }`}
         type="button"
         aria-label="save or delete button"
         onClick={() => handleCardButton(currentCard)}
       />
-      <p className="newsCard__help">
-        {isMain ? (!loggedIn && "Sign in to save articles") : ("Remove from saved")}
-      </p>
-      <img
-        className="newsCard__image"
-        src={image}
-        alt={title}
-      />
+      {isMain ? (
+        !loggedIn && <p className="newsCard__help">Sign in to save articles</p>
+      ) : (
+        <p className="newsCard__help">Remove from saved</p>
+      )}
+      <img className="newsCard__image" src={image} alt={title} />
       <div className="newsCard__text-container">
         <p className="newsCard__date newsCard__text-container-item">
           {shortDate}
@@ -85,9 +84,7 @@ function deleteCardFromSaved(currentCard) {
         <h2 className="newsCard__title newsCard__text-container-item">
           {title}
         </h2>
-        <p className="newsCard__text newsCard__text-container-item">
-          {text}
-        </p>
+        <p className="newsCard__text newsCard__text-container-item">{text}</p>
         <p className="newsCard__source newsCard__text-container-item">
           {source}
         </p>
