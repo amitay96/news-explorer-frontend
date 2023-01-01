31a11568b14abe20afc0692abd94add8ed18dcc1
Amitay Kagan <Amitay96@gmail.com>
2023-01-01 15:21:07 +0200

    feature: no authorization redirect login

--- src/components/ProtectedRoute/ProtectedRoute.js
+++ src/components/ProtectedRoute/ProtectedRoute.js
@@ -1,11 +1,19 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useStore } from "../../contexts/GlobalContext";
+import { usePopup } from "../../contexts/PopupContext";
+
 
 const ProtectedRoute = ({ children }) => {
   const { loggedIn } = useStore().UserActions;
+  const { openPopup } = usePopup();
 
-  return loggedIn ? children : <Navigate to="/" />;
+  const backHome = () => {
+    openPopup("signin");
+    return <Navigate to="/" />
+  }
+
+  return loggedIn ? children : backHome();
 };
 
 export default ProtectedRoute;
